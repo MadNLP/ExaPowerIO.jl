725771c6193302bb8e3dd88646ac225522e0cc9d
Archim J <archimj@proton.me>
2025-07-26 01:16:29 -0400

    reworked benchmarks. results: [ Info: Running ExaPowerIO:   1.327 s (86402 allocations: 809.27 MiB) [ Info: Running JLD2:   61.522 ms (460398 allocations: 174.76 MiB) [ Info: Running PowerModels:   36.598 s (531914384 allocations: 18.62 GiB)

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -1,70 +1,70 @@
 CASES = [
-    (Float64, "pglib_opf_case3_lmbd.m"),
-    (Float64, "pglib_opf_case1803_snem.m"),
-    (Float64, "pglib_opf_case118_ieee.m"),
-    (Float64, "pglib_opf_case1888_rte.m"),
-    (Float64, "pglib_opf_case13659_pegase.m"),
-    (Float64, "pglib_opf_case1354_pegase.m"),
-    (Float64, "pglib_opf_case10000_goc.m"),
-    (Float64, "pglib_opf_case10192_epigrids.m"),
-    (Float64, "pglib_opf_case10480_goc.m"),
-    (Float64, "pglib_opf_case14_ieee.m"),
-    (Float64, "pglib_opf_case162_ieee_dtc.m"),
-    (Float64, "pglib_opf_case179_goc.m"),
-    (Float64, "pglib_opf_case19402_goc.m"),
-    (Float64, "pglib_opf_case1951_rte.m"),
-    (Float64, "pglib_opf_case197_snem.m"),
-    (Float64, "pglib_opf_case2000_goc.m"),
-    (Float64, "pglib_opf_case200_activ.m"),
-    (Float64, "pglib_opf_case20758_epigrids.m"),
-    (Float64, "pglib_opf_case2312_goc.m"),
-    (Float64, "pglib_opf_case2383wp_k.m"),
-    (Float64, "pglib_opf_case240_pserc.m"),
-    (Float64, "pglib_opf_case24464_goc.m"),
-    (Float64, "pglib_opf_case24_ieee_rts.m"),
-    (Float64, "pglib_opf_case2736sp_k.m"),
-    (Float64, "pglib_opf_case2737sop_k.m"),
-    (Float64, "pglib_opf_case2742_goc.m"),
-    (Float64, "pglib_opf_case2746wop_k.m"),
-    (Float64, "pglib_opf_case2746wp_k.m"),
-    (Float64, "pglib_opf_case2848_rte.m"),
-    (Float64, "pglib_opf_case2853_sdet.m"),
-    (Float64, "pglib_opf_case2868_rte.m"),
-    (Float64, "pglib_opf_case2869_pegase.m"),
-    (Float64, "pglib_opf_case30000_goc.m"),
-    (Float64, "pglib_opf_case300_ieee.m"),
-    (Float64, "pglib_opf_case3012wp_k.m"),
-    (Float64, "pglib_opf_case3022_goc.m"),
-    (Float64, "pglib_opf_case30_as.m"),
-    (Float64, "pglib_opf_case30_ieee.m"),
-    (Float64, "pglib_opf_case3120sp_k.m"),
-    (Float64, "pglib_opf_case3375wp_k.m"),
-    (Float64, "pglib_opf_case3970_goc.m"),
-    (Float64, "pglib_opf_case39_epri.m"),
-    (Float64, "pglib_opf_case4020_goc.m"),
-    (Float64, "pglib_opf_case4601_goc.m"),
-    (Float64, "pglib_opf_case4619_goc.m"),
-    (Float64, "pglib_opf_case4661_sdet.m"),
-    (Float64, "pglib_opf_case4837_goc.m"),
-    (Float64, "pglib_opf_case4917_goc.m"),
-    (Float64, "pglib_opf_case500_goc.m"),
-    (Float64, "pglib_opf_case5658_epigrids.m"),
-    (Float64, "pglib_opf_case57_ieee.m"),
-    (Float64, "pglib_opf_case588_sdet.m"),
-    (Float64, "pglib_opf_case5_pjm.m"),
-    (Float64, "pglib_opf_case60_c.m"),
-    (Float64, "pglib_opf_case6468_rte.m"),
-    (Float64, "pglib_opf_case6470_rte.m"),
-    (Float64, "pglib_opf_case6495_rte.m"),
-    (Float64, "pglib_opf_case6515_rte.m"),
-    (Float64, "pglib_opf_case7336_epigrids.m"),
-    (Float64, "pglib_opf_case73_ieee_rts.m"),
-    (Float64, "pglib_opf_case78484_epigrids.m"),
-    (Float64, "pglib_opf_case793_goc.m"),
-    (Float64, "pglib_opf_case8387_pegase.m"),
-    (Float64, "pglib_opf_case89_pegase.m"),
-    (Float64, "pglib_opf_case9241_pegase.m"),
-    (Float64, "pglib_opf_case9591_goc.m"),
+    (Float64, "pglib_opf_case3_lmbd"),
+    (Float64, "pglib_opf_case1803_snem"),
+    (Float64, "pglib_opf_case118_ieee"),
+    (Float64, "pglib_opf_case1888_rte"),
+    (Float64, "pglib_opf_case13659_pegase"),
+    (Float64, "pglib_opf_case1354_pegase"),
+    (Float64, "pglib_opf_case10000_goc"),
+    (Float64, "pglib_opf_case10192_epigrids"),
+    (Float64, "pglib_opf_case10480_goc"),
+    (Float64, "pglib_opf_case14_ieee"),
+    (Float64, "pglib_opf_case162_ieee_dtc"),
+    (Float64, "pglib_opf_case179_goc"),
+    (Float64, "pglib_opf_case19402_goc"),
+    (Float64, "pglib_opf_case1951_rte"),
+    (Float64, "pglib_opf_case197_snem"),
+    (Float64, "pglib_opf_case2000_goc"),
+    (Float64, "pglib_opf_case200_activ"),
+    (Float64, "pglib_opf_case20758_epigrids"),
+    (Float64, "pglib_opf_case2312_goc"),
+    (Float64, "pglib_opf_case2383wp_k"),
+    (Float64, "pglib_opf_case240_pserc"),
+    (Float64, "pglib_opf_case24464_goc"),
+    (Float64, "pglib_opf_case24_ieee_rts"),
+    (Float64, "pglib_opf_case2736sp_k"),
+    (Float64, "pglib_opf_case2737sop_k"),
+    (Float64, "pglib_opf_case2742_goc"),
+    (Float64, "pglib_opf_case2746wop_k"),
+    (Float64, "pglib_opf_case2746wp_k"),
+    (Float64, "pglib_opf_case2848_rte"),
+    (Float64, "pglib_opf_case2853_sdet"),
+    (Float64, "pglib_opf_case2868_rte"),
+    (Float64, "pglib_opf_case2869_pegase"),
+    (Float64, "pglib_opf_case30000_goc"),
+    (Float64, "pglib_opf_case300_ieee"),
+    (Float64, "pglib_opf_case3012wp_k"),
+    (Float64, "pglib_opf_case3022_goc"),
+    (Float64, "pglib_opf_case30_as"),
+    (Float64, "pglib_opf_case30_ieee"),
+    (Float64, "pglib_opf_case3120sp_k"),
+    (Float64, "pglib_opf_case3375wp_k"),
+    (Float64, "pglib_opf_case3970_goc"),
+    (Float64, "pglib_opf_case39_epri"),
+    (Float64, "pglib_opf_case4020_goc"),
+    (Float64, "pglib_opf_case4601_goc"),
+    (Float64, "pglib_opf_case4619_goc"),
+    (Float64, "pglib_opf_case4661_sdet"),
+    (Float64, "pglib_opf_case4837_goc"),
+    (Float64, "pglib_opf_case4917_goc"),
+    (Float64, "pglib_opf_case500_goc"),
+    (Float64, "pglib_opf_case5658_epigrids"),
+    (Float64, "pglib_opf_case57_ieee"),
+    (Float64, "pglib_opf_case588_sdet"),
+    (Float64, "pglib_opf_case5_pjm"),
+    (Float64, "pglib_opf_case60_c"),
+    (Float64, "pglib_opf_case6468_rte"),
+    (Float64, "pglib_opf_case6470_rte"),
+    (Float64, "pglib_opf_case6495_rte"),
+    (Float64, "pglib_opf_case6515_rte"),
+    (Float64, "pglib_opf_case7336_epigrids"),
+    (Float64, "pglib_opf_case73_ieee_rts"),
+    (Float64, "pglib_opf_case78484_epigrids"),
+    (Float64, "pglib_opf_case793_goc"),
+    (Float64, "pglib_opf_case8387_pegase"),
+    (Float64, "pglib_opf_case89_pegase"),
+    (Float64, "pglib_opf_case9241_pegase"),
+    (Float64, "pglib_opf_case9591_goc"),
 ]
 
 using Pkg
@@ -72,7 +72,7 @@ using Pkg
 Pkg.activate(@__DIR__)
 Pkg.develop(path=dirname(@__DIR__))
 
-using ExaPowerIO, BenchmarkTools, PowerModels, PGLib, Profile, PProf, Logging
+using ExaPowerIO, BenchmarkTools, PowerModels, PGLib, Profile, PProf, Logging, JLD2
 
 PowerModels.silence()
 
@@ -84,40 +84,64 @@ for (i, arg) in enumerate(ARGS)
         break
     end
 end
-COMPARE = "--compare" in ARGS
+COMPARE_PM = "--compare-pm" in ARGS
+COMPARE_JLD2 = "--compare-jld2" in ARGS
 PROFILE = "--profile" in ARGS
-@info "Running benchmarks with num-samples: $NUM_SAMPLES, compare to PowerModels: $COMPARE, profile: $PROFILE"
-
-function run_pm(dataset :: String)
-    path = joinpath(PGLib.PGLib_opf, dataset)
-    pm_output = PowerModels.parse_file(path)
-    PowerModels.standardize_cost_terms!(pm_output, order = 2)
-    PowerModels.calc_thermal_limits!(pm_output)
-end
+@info """
+Running benchmarks with settings:
+num-samples: $NUM_SAMPLES
+compare to PowerModels: $COMPARE_PM
+compare to JLD2: $COMPARE_JLD2
+output pprof file: $PROFILE
+"""
 
 datadir = "../data/"
-function display_btimed(btimed :: NamedTuple)
-    display(btimed[(:time, :bytes, :alloc, :gctime)])
+
+function run_exapower!()
+    data = Vector{NamedTuple}(undef, length(CASES))
+    for (i, (type, dataset)) in enumerate(CASES)
+        data[i] = ExaPowerIO.parse_pglib(type, Vector, dataset; out_type=NamedTuple)
+    end
+    data
 end
 
-if PROFILE
-    Profile.clear()
-    global_logger(ConsoleLogger(stderr, Logging.Warn))
-        @profile begin
-            for (type, dataset) in CASES
-                ExaPowerIO.parse_pglib(type, Vector, dataset; out_type=NamedTuple)
-            end
-        end
-    pprof()
-else
-    for (type, dataset) in CASES
-        @info "ExaPowerIO.jl " * dataset
-        nt = @btimed ExaPowerIO.parse_pglib(type, Vector, $dataset; out_type=NamedTuple) samples=NUM_SAMPLES
-        display_btimed(nt)
-        if COMPARE
-            @info "PowerModels.jl " * dataset
-            nt = @btimed run_pm($dataset) samples=NUM_SAMPLES
-            display_btimed(nt)
-        end
+function run_jld2()
+    for (_, dataset) in CASES
+        JLD2.load(joinpath(datadir, dataset * ".jld2"))
     end
+end
+
+function run_pm()
+    for (_, dataset) in CASES
+        path = joinpath(PGLib.PGLib_opf, dataset * ".m")
+        pm_output = PowerModels.parse_file(path)
+        PowerModels.standardize_cost_terms!(pm_output, order = 2)
+        PowerModels.calc_thermal_limits!(pm_output)
+    end
+end
+
+data = []
+Profile.clear()
+
+@info "Running ExaPowerIO:"
+global_logger(ConsoleLogger(stderr, Logging.Warn))
+if PROFILE
+    @btime (global output = @profile run_exapower!()) teardown = (global data = output)
+else
+    @btime (global output = run_exapower!()) teardown = (global data = output)
+end
+@info ("hi", length(data))
+global_logger(ConsoleLogger(stderr, Logging.Info))
+
+if COMPARE_JLD2
+    for (i, (type, dataset)) in enumerate(CASES)
+        JLD2.save(joinpath(datadir, dataset * ".jld2"), Dict("data" => data[i]))
+    end
+    @info "Running JLD2:"
+    @btime run_jld2()
+end
+
+if COMPARE_PM
+    @info "Running PowerModels:"
+    @btime run_pm()
 end
